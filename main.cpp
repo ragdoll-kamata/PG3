2afc812ae61f67dc0dd97797928b941dce0b49fd
オジマアキト <k023g0097@g.neec.ac.jp>
2024-10-08 12:09:34 +0900

    01_04完了

--- main.cpp
+++ main.cpp
@@ -2,12 +2,10 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <functional>
 
-typedef void (*T)(int);
-void Answer(int a) {
-	printf("%d!\n", a);
-}
-void SetWait(T a, int answer) {
+
+void SetTimeout(std::function<void(int)> a, int answer) {
 	for (int i = 0; i < 3; i++) {
 		printf("・");
 		Sleep(1000);
@@ -20,11 +18,12 @@ int main(){
 	unsigned int currentTime = time(nullptr);
 	srand(currentTime);
 	int answer = rand() % 6 + 1;
+	std::function<void(int)> fAnswer = [](int a) { printf("%d!\n", a); };
 	int a;
 	printf("サイコロの出目が偶数か奇数か当ててね！\n偶数なら0\n奇数なら1\n");
 	scanf_s("%d", &a);
 	printf("\n答えは");
-	SetWait(Answer, answer);
+	SetTimeout(fAnswer, answer);
 	if (answer % 2 == a) {
 		printf("正解!");
 	} else {
