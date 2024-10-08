94d245a393e8a113e8dc637278434091264c8938
オジマアキト <k023g0097@g.neec.ac.jp>
2024-10-08 12:00:03 +0900

    01_03完了

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-int Saiki(int a, int b) {
-	if (a > 0) {
-		return b + Saiki(a - 1, b * 2 - 50);
+typedef void (*T)(int);
+void Answer(int a) {
+	printf("%d!\n", a);
+}
+void SetWait(T a, int answer) {
+	for (int i = 0; i < 3; i++) {
+		printf("・");
+		Sleep(1000);
 	}
-	return b;
+	
+	a(answer);
 }
 
 int main(){
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
+	int answer = rand() % 6 + 1;
 	int a;
-
-	printf("何時間働いた?");
+	printf("サイコロの出目が偶数か奇数か当ててね！\n偶数なら0\n奇数なら1\n");
 	scanf_s("%d", &a);
-	printf("一般的な賃金 : %d\n再帰的な賃金 : %d", 1072 * a, Saiki(a, 100));
-
+	printf("\n答えは");
+	SetWait(Answer, answer);
+	if (answer % 2 == a) {
+		printf("正解!");
+	} else {
+		printf("不正解");
+	}
+	
 	return 0;
 }
