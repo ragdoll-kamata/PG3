5ec3953ee9454ad3a09aca24fcf9ef6ae6f53159
オジマアキト <k023g0097@g.neec.ac.jp>
2024-10-08 10:40:27 +0900

    01_02完了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
+
+int Saiki(int a, int b, int c) {
+	if (a > 0) {
+		return Saiki(a - 1, b * 2 - 50, b + c);
+	}
+	return c;
+}
+
 int main(){
-	
-	char str[] = "ソ";
-	printf("%s", str);
+	int a;
+
+	printf("何時間働いた?");
+	scanf_s("%d", &a);
+	printf("一般的な賃金 : %d\n再帰的な賃金 : %d", 1072 * a, Saiki(a, 100, 0));
 
 	return 0;
 }
