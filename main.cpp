a6b1786a30559624c42e5db5b93e6c9e64a1b5c1
オジマアキト <k023g0097@g.neec.ac.jp>
2024-10-29 15:31:57 +0900

    02_02完了

--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <stdio.h>
-#include "Enemy.h"
 
+template <typename T1, typename T2>
+
+class Math {
+private:
+	T1 a;
+	T2 b;
+public:
+	Math(T1 a, T2 b) : a(a), b(b) {}
+
+	T1 Min() {
+		if (a <= static_cast<T1>(b)) {
+			return static_cast<T1>(a);
+		}
+		return  static_cast<T1>(b);
+	}
+};
 
 int main(){
-	int a;
-	Enemy enemy;
+	Math<int, int> a(10, 20);
+	Math<float, float> b(1.2f, 0.4f);
+	Math<double, double> c(0.01, 0.023);
+	Math<int, float> d(10, 1.6f);
+	Math<float, double> e(11.1f, 22.2);
+	Math<double, int> f(25.6, 64);
 
-	while (true) {
 
-		enemy.Updata();
+	printf("a = %d\n", a.Min());
+	printf("b = %f\n", b.Min());
+	printf("c = %lf\n", c.Min());
+	printf("d = %d\n", d.Min());
+	printf("e = %f\n", e.Min());
+	printf("f = %lf\n", f.Min());
 
-		printf("0で次のフェーズ、1でループを抜ける\n");
-		scanf_s("%d", &a);
-		if (a == 1) {
-			break;
-		}
-	}
+
 	return 0;
 }
