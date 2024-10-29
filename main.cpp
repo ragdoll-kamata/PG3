95fdd76f918ec787ae4e70e54b5ce327bfb6d37e
オジマアキト <k023g0097@g.neec.ac.jp>
2024-10-29 15:40:12 +0900

    02_02完了

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 
 template <typename T1, typename T2>
 
@@ -10,28 +11,27 @@ public:
 	Math(T1 a, T2 b) : a(a), b(b) {}
 
 	T1 Min() {
-		if (a <= static_cast<T1>(b)) {
+		if (a <= b) {
 			return static_cast<T1>(a);
 		}
-		return  static_cast<T1>(b);
+		return  static_cast<T2>(b);
 	}
 };
 
 int main(){
-	Math<int, int> a(10, 20);
-	Math<float, float> b(1.2f, 0.4f);
-	Math<double, double> c(0.01, 0.023);
-	Math<int, float> d(10, 1.6f);
-	Math<float, double> e(11.1f, 22.2);
-	Math<double, int> f(25.6, 64);
+	Math<int, float> a(10, 50.1f);
+	Math<int, double> b(80, 13.23);
+	Math<float, int> c(2.4f, 9);
+	Math<float, double> d(11.9f, 3.5);
+	Math<double, int> e(66.6, 33);
+	Math<double, float> f(25.6, 51.2f);
 
-
-	printf("a = %d\n", a.Min());
-	printf("b = %f\n", b.Min());
-	printf("c = %lf\n", c.Min());
-	printf("d = %d\n", d.Min());
-	printf("e = %f\n", e.Min());
-	printf("f = %lf\n", f.Min());
+	std::cout << "int(10)　と float(50.1f) を比べて小さい数字を表す：" << a.Min() << std::endl;
+	std::cout << "int(80)　と double(13.23) を比べて小さい数字を表す：" << b.Min() << std::endl;
+	std::cout << "float(2.4f)　と int(9) を比べて小さい数字を表す：" << c.Min() << std::endl;
+	std::cout << "float(11.9f)　と double(3.5) を比べて小さい数字を表す：" << d.Min() << std::endl;
+	std::cout << "double(66.6)　と int(33) を比べて小さい数字を表す：" << e.Min() << std::endl;
+	std::cout << "double(25.6)　と float(51.2f) を比べて小さい数字を表す：" << f.Min() << std::endl;
 
 
 	return 0;
